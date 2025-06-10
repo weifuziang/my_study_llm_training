6dae52ad456f52e2ab0efb6374883bd72339b6a2
zhangweiwei <zhangweiwei@myai.tech>
2025-06-10 08:32:44 +0800

    20225060212424_数据结构与算法——数组构建2

--- data_structures_algorithms/CommonlyStructures.py
+++ data_structures_algorithms/CommonlyStructures.py
@@ -8,9 +8,38 @@
 
 """
 数组 
-基本概念：线性数据结构，将相同类型的元素按照顺序存储在连续的内存空间中，每个元素都有一个索引；
-关注点：连续存储、开始索引、数据类型；
-注释：python中，并没有严格意义上的”数组“，用list模型、array模块的数组、numpy库的ndarray
+1. 基本概念：线性数据结构，将相同类型的元素按照顺序存储在连续的内存空间中，每个元素都有一个索引；
+2. 关注点：连续存储、***开始索引或者末尾索引（可以知道任意元素位置）***、数据类型；
+3. 注释：python中，并没有严格意义上的”数组“，用list模型、array模块的数组、numpy库的ndarray
+
+***数组封装核心***
+1. 初始化：
+    用什么样的存储？ --> list作为核心存储（self.__items=[0]）；
+    数组的初始化容量 --> self.__capacity=8 、 self.__items=[0] * self.__capacity;
+    数组的初始化大小 --> self.size=0
+    
+2. 插入、删除操作
+   主逻辑：数组元素挨个移动，并覆盖
+   插入：逻辑 --> 从末尾索引依次向后移动一位，直到插入索引（index）的位置，则停止移动，并将self.__items[index]=insertValue进行赋值
+        实现 --> for i in range(self.__size,index,-1): self.__items[i]=self.__items[i-1]
+                self.__size+=1  
+                self.__items[index]=insertValue;
+        注释 --> 由于只有知道开始或者末尾索引才能知道元素的位置,同时在插入的时候也要注意数组的capacity是否够用，若不够用则需要进行数组的扩容；
+        
+   删除：逻辑 --> 从删除索引位置（deleteIndex）开始依次向前移动一位（将删除索引覆盖掉，并将后面的元素都往前移动）
+        实现 --> for i in range(deleteIndex,self.__size,1):self.__items[i]=self.__items[i+1]
+                self.__size-=1
+3. 扩容
+    主逻辑：创建新的存储（self.new__items=[0]*self.__capacity*2）,将旧存储（self.__items)元素挨个赋值，
+           然后，将新存储地址赋值给旧存储（self.items=self.new__items）
+           最后，数组容量进行真是变更（self.capacity=self.__capacity*2）
+           
+    实现：扩容 --> self.new__items=[0]*self.__capacity*2）
+         旧元素迁移 --> for i in range(self.__size): self.new__items[i]=self.__items[i]
+         地址回签 --> self.__items=self.new__items
+         数组容量变更 --> self.capacity=self.__capacity*2
+           
+
 """
 
 
@@ -65,8 +94,43 @@ class Array:
         self.__size += 1
 
     def append(self, item):
-        """末尾插入元素"""
+        """末尾插入元素：同样也调用insert方法，目的是为了统一性，如果不调用，直接用list的append()方法，将不会出现数组扩容操作"""
         self.insert(self.__size, item)
+
+    def remove(self, index):
+        """
+        删除元素
+        :param index:
+        :return:
+        """
+        if index < 0 or index > self.__size:
+            raise IndexError("Index out of range")
+        for i in range(index, self.__size):
+            self.__items[i] = self.__items[i + 1]  # 操作数组依次向右移动，index位置的元素会直接被index+1（即 i+1）覆盖掉
+        self.__size -= 1
+
+    def set(self, index, item):
+        """修改元素"""
+        if index < 0 or index > self.__size:
+            raise IndexError("Index out of range")
+        self.__items[index] = item
+
+    def get(self, index):
+        """访问元素"""
+        if index < 0 or index >= self.__size:
+            raise IndexError
+        return self.__items[index]
+
+    def find(self, item):
+        """查看元素"""
+        for i in range(self.__size):
+            if self.__items[i] == item:
+                return i
+        return -1
+
+    def forEach(self):
+        for i in range(self.__size):
+            print(self.__items[i])
 
 
 arr = Array()
@@ -77,6 +141,11 @@ arr.append(6)
 arr.append(9)
 print(arr.size)
 print(arr.__str__())
+print(arr.get(2))
+# arr.remove(0)
+arr.set(0, 100)
+arr.insert(0, 101)
+arr.forEach()
 # arr.insert(0, 10)
 # print(arr.__str__())
 
