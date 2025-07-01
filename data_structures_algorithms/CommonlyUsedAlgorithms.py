08ef3367abf7a3eeff15fbb2d124850650c1df9b
zhangweiwei <zhangweiwei@myai.tech>
2025-07-02 07:49:19 +0800

    2022507020746_数据结构与算法——贪心算法_分发糖果

--- data_structures_algorithms/CommonlyUsedAlgorithms.py
+++ data_structures_algorithms/CommonlyUsedAlgorithms.py
@@ -701,10 +701,40 @@ def maxinumSwap(nums):
             list_nums[i] , list_nums[max_index] = list_nums[max_index] ,list_nums[i]
     return result
 
-if __name__ == '__main__':
-    nums = 2736
-    print(maxinumSwap(nums))
+# if __name__ == '__main__':
+#     nums = 2736
+#     print(maxinumSwap(nums))
 
 
+###贪心算法应用---分发糖果（待了解）
+"""
+题目：
+n个孩子站成一排。用一个整数数组ratings表示每个孩子的评分。按照以下要求，给这些孩子分发糖果：
+	每个孩子至少分配到1个糖果。
+	相邻两个孩子评分更高的孩子会获得更多的糖果。
+给每个孩子分发糖果，计算并返回需要准备的最少糖果数目。
+
+
+"""
+
+def candy(ratings):
+    n = len(ratings)
+
+    # 每个孩子先分发1个糖果
+    candy_num = [1] * len(ratings)
+
+    # 从左向右遍历，如果右边评分更高，则右边孩子的糖果数改为左边孩子的糖果数+1
+    for pos in range(n - 1):
+        if ratings[pos] < ratings[pos + 1]:
+            candy_num[pos + 1] = candy_num[pos] + 1
+
+    # 从右向左遍历，如果左边评分更高，则左边孩子的糖果数改为右边孩子的糖果数+1
+    total_candies = candy_num[-1]
+    for pos in range(n - 2, -1, -1):
+        if ratings[pos] > ratings[pos + 1]:
+            if candy_num[pos] <= candy_num[pos + 1]:
+                candy_num[pos] = candy_num[pos + 1] + 1
+        total_candies += candy_num[pos]
+    return total_candies
 
 
