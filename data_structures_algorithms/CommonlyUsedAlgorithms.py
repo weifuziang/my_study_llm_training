34e78f62aabd22fae1088d47e1a2fe32e279c674
zhangweiwei <zhangweiwei@myai.tech>
2025-07-01 22:19:46 +0800

    2022507012219120_数据结构与算法——回溯算法_全排列2

--- data_structures_algorithms/CommonlyUsedAlgorithms.py
+++ data_structures_algorithms/CommonlyUsedAlgorithms.py
@@ -574,14 +574,37 @@ def knapsack(weights, values, W):
         d. 回溯：当某条路径不满足条件或无法继续是，回到上一步，尝试其他可能得选择；
 
 """
-###回溯算法应用案例---全排列
+###回溯算法应用案例---全排列（了解知道即可）
 """
 题目:
 现有一个不含重复数字的数组nums ，返回其所有可能的全排列。
 示例：
 	输入：nums = [1,2,3]
 	输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
-
-
 """
+
+def permute(nums):
+    result = []
+
+    def backtrack(start):
+        # 到达末尾，将此时排列结果添加到最终结果中
+        if start == len(nums):
+            result.append(nums[:])
+            return
+
+        for i in range(start, len(nums)):
+            # 选取当前位置的元素：将要选取的元素与此位置的元素互换
+            if start != i:
+                nums[start], nums[i] = nums[i], nums[start]
+            # 递归处理下一个位置的元素
+            backtrack(start + 1)
+            # ***回溯，恢复原始数组
+            if start != i:
+                nums[start], nums[i] = nums[i], nums[start]
+
+    backtrack(0)
+    return result
+if __name__ == '__main__':
+    print(permute([1,2,3,4]))
+
 
